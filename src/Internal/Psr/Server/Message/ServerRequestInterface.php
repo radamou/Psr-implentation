f587b4fcfc2578440fe111ed191ba4af59bb12cc
Ramatou Adamou <ramatou.adamou@aptushealth.com>
2019-04-10 18:13:08 +0200

    add request specification

--- src/Internal/Psr/Server/Message/ServerRequestInterface.php
+++ src/Internal/Psr/Server/Message/ServerRequestInterface.php
@@ -9,5 +9,44 @@ namespace App\Internal\Psr\Server\Message;
  */
 interface ServerRequestInterface
 {
+    /**
+     * Set a header value (associative array)
+     * include case insensitive header field names (add to lower case before setting it)
+     *
+     * headers are retrieved by name
+     */
+    public function withHeader(string $header, string $value): self;
 
+    /**
+     *
+     * return the Host header by default
+     */
+    public function withUri(UriInterface $uri): string;
+
+    /**
+     * retrieve a header value as a string by it's name
+     * include case insensitive header field names (add to lower case before getting it)
+     * value like Set-Cookie can't be retrieved with this function, use getHeader instead
+     *
+     * headers are retrieved by name (concatenate with comma)
+     *
+     * @example $header contains: 'bar, baz'
+     *
+     */
+    public function getHeaderLine(string $header): string;
+
+    /**
+     * retrieve a header value as an array by it's name
+     * include case insensitive header field names (add to lower case before getting it)
+     *
+     * headers are retrieved by name (concatenate with comma)
+     *
+     * @example $header contains: ['bar, baz']
+     */
+    public function getHeader(): array ;
+
+    /**
+     * retrieve all headers
+     */
+    public function getHeaders(): array;
 }
